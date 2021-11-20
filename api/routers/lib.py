ff047cbec78e170d231c1c91c1550796f4f112ba
junyi <47441677+immanuelhume@users.noreply.github.com>
2021-11-20 21:40:10 +0800

    Add x-api-key authentication

---
+++ api/routers/lib.py
@@ -0,0 +1,15 @@
+from fastapi import Depends, Header, HTTPException, status
+from sqlalchemy.connectors import Connector
+
+from api.auth import validate_api_key
+from api.db.db import get_db
+
+
+async def verify_api_key(x_api_key: str = Header(...), c: Connector = Depends(get_db)):
+    if not validate_api_key(c, x_api_key):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="X-API-KEY header is invalid",
+        )
+
+    return x_api_key
