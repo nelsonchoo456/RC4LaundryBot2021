b1acd2cd21d92e19bd19b9841f8c6c898b834401
heyzec <61238538+heyzec@users.noreply.github.com>
2021-12-07 23:09:00 +0800

    Tele bot: Minor changes

--- telegrambot/telegram-main.py
+++ telegrambot/telegram-main.py
@@ -42,12 +42,12 @@ logging.basicConfig(
 # Global constants
 LAUNDRY_LEVELS = [5, 8, 11, 14, 17]
 WASHERS_INFO = {
-    "washer1": "Washer 1 (Coin)",
-    "washer2": "Washer 2 (PayLah)"
+    ":1": "Washer 1 (Coin)",
+    ":2": "Washer 2 (PayLah)"
 }
 DRYERS_INFO = {
-    "dryer1": "Dryer 1 (Coin)",
-    "dryer2": "Dryer 2 (PayLah)"
+    ":3": "Dryer 1 (Coin)",
+    ":4": "Dryer 2 (PayLah)"
 }
 MACHINES_INFO = {**WASHERS_INFO, **DRYERS_INFO}
 MAIN_MENU, SHOWING, SET_REMINDER = range(3)
@@ -62,28 +62,22 @@ def build_menu(buttons, n_cols, header_buttons=None, footer_buttons=None):
         menu.append(footer_buttons)
     return InlineKeyboardMarkup(menu)
 
-# def help_command(update: Update, _: CallbackContext) -> None:
-#    """Send a message when the command /help is issued."""
-#    update.message.reply_text('Help!')
 
-
-# def echo(update: Update, _: CallbackContext) -> None:
-#    """Echo the user message."""
-#
-#    update.message.reply_text(f"you said {update.message.text}")
-
-
-def start(update: Update, _: CallbackContext) -> int:
+def start(update: Update, context) -> int:
     """Send a message when the command /start is issued."""
+
+    # Initialise variables needed for later use in one place here
+    user_data = context.user_data
+    user_data["reminder_memory"] = {}    # Needed when user chooses which machines to get reminder
+    user_data["level"] = None            # Store the current level that the user is requesting info from
+    user_data["msg_id_status"] = None    # msg_id of the status msg - needed to edit the message later on
+    user_data["msg_id_reminder"] = None  # msg_id of the reminder msg - needed to delete it later on
 
     text = f"""Heyyo {update.effective_user.username}! I am RC4's Laundry Bot. <i>I am currently in [BETA] mode</i>.
 
 Send me a /view_status to view some status of machines!"""
 
     update.message.reply_text(text, parse_mode="HTML")
-
-    # TO DO: Initalise variables in context.user_data here to make code more understandable
-
 
     return MAIN_MENU
 
@@ -100,6 +94,7 @@ def ask_level(update: Update, _: CallbackContext) -> int:
             text=f"Level {level}", callback_data=f"check_L{level}"
         )
         level_buttons.append(buttons)
+
     update.message.reply_text(
         text=level_text, reply_markup=build_menu(level_buttons, 1), parse_mode="HTML"
     )
@@ -107,10 +102,13 @@ def ask_level(update: Update, _: CallbackContext) -> int:
 
 
 
-def level_status(update, context) -> int:
-    query = update.callback_query
+def level_status(update, context): # -> typing.Union[int, None]
 
-    if "check_L" in query.data:
+    query = update.callback_query
+    # True if initiated by user giving bot a level number, False if otherwise initiated by update request
+    is_first_request = "check_L" in query.data
+
+    if is_first_request:
         level = int(query.data.split("_L")[1])
         context.user_data['level'] = level
     else:
@@ -166,12 +164,11 @@ Last updated: {current_time}\n"""
     ]
 
 
-    # Use context.user_data is not None instead
-    if "status" in context.user_data:
-        # Remove the inline buttons for the previous message
+    if context.user_data["msg_id_status"] is not None:
+        # If there is previous message, remove the inline buttons for that message
         context.bot.edit_message_reply_markup(
                 chat_id=update.effective_chat.id,
-                message_id=context.user_data["status"],
+                message_id=context.user_data["msg_id_status"],
                 reply_markup=None
         )
 
@@ -179,45 +176,44 @@ Last updated: {current_time}\n"""
     message = update.callback_query.edit_message_text(
         text=text, reply_markup=InlineKeyboardMarkup(layout), parse_mode="HTML"
     )
+    context.user_data["msg_id_status"] = message.message_id
 
-    context.user_data["status"] = message.message_id
-
-    if "check_L" in query.data:
+    if is_first_request:
         query.answer()
+        return SHOWING
     else:
         query.answer("Updated!")
-
-
-    # TO DO: Conditionally return status. (Don't change status if its just an update, but if new msg then return SHOWING)
-    return SHOWING
+        return None  # Don't change the conversation state!
 
 
 def set_reminder(update, context) -> int:
     """For user to select which machines for reminder"""
 
+    # TO DO: only allow user to press this once at a time!!
+
     query = update.callback_query
+    first_run = query.data == "reminder"
+    level = context.user_data["level"]
+    memory = context.user_data["reminder_memory"]
 
-    if query.data == "reminder":
-        first_run = True
+    if len(memory) == 0:
+    # print(context.user_data["reminder_memory"])
+    # if context.user_data["reminder_memory"] is None:
+        memory = {f"{level}{k}": False for k in MACHINES_INFO.keys()}
+        context.user_data["reminder_memory"] = memory
     else:
-        first_run = False
-
-    if "memory" not in context.user_data:
-        memory = {k: False for k, v in MACHINES_INFO.items()}
-        context.user_data["memory"] = memory
-    else:
-        memory = context.user_data["memory"]
+        memory = context.user_data["reminder_memory"]
         if not first_run:
             memory[query.data] = not memory[query.data]
 
     print(memory)
 
     layout = [
-        [InlineKeyboardButton(v + (" ✓" if memory[k] else ""), callback_data=k) for k, v in WASHERS_INFO.items()],
-        [InlineKeyboardButton(v + (" ✓" if memory[k] else ""), callback_data=k) for k, v in DRYERS_INFO.items()]
+        [InlineKeyboardButton(v + (" ✓" if memory[f"{level}{k}"] else ""), callback_data=f"{level}{k}") for k, v in WASHERS_INFO.items()],
+        [InlineKeyboardButton(v + (" ✓" if memory[f"{level}{k}"] else ""), callback_data=f"{level}{k}") for k, v in DRYERS_INFO.items()]
     ]
 
-    text = """Select which machines you'd like to be reminded when complete.
+    text = f"""<b>[Level {level}]</b> Select which machines you'd like to be reminded when complete.
 Send /done when you're done!"""
 
     # TO DO: If reminder has already been set, send a more meaningful message instead
@@ -227,11 +223,12 @@ Send /done when you're done!"""
             update.effective_chat["id"],
             text,
             reply_markup=InlineKeyboardMarkup(layout),
+            parse_mode="HTML"
         )
-        context.user_data['reminder_msg_id'] = message.message_id
+        context.user_data['msg_id_reminder'] = message.message_id
     else:
         query.edit_message_reply_markup(
-            reply_markup=InlineKeyboardMarkup(layout),
+            reply_markup=InlineKeyboardMarkup(layout)
         )
 
     query.answer()
@@ -239,33 +236,65 @@ Send /done when you're done!"""
 
 
 def reminder_done(update, context) -> int:
-    context.bot.delete_message(update.effective_chat.id, context.user_data['reminder_msg_id'])
 
-    memory = context.user_data['memory']
-    if any(context.user_data['memory'].values()):
-        update.message.reply_text("Okay! I'll remind you when machine's ready.")
-    else:
-        # If the user did not tick any of the machines
-        update.message.reply_text("Hey you didn't pick any of the machines!")
-
+    memory = context.user_data['reminder_memory']
     user_id = update.effective_user.id
-
 
     reminders = context.bot_data['reminders']
     for k, v in memory.items():
         if v:
             if k not in reminders:
                 reminders[k] = [user_id]
-
     print(reminders)
 
 
+    context.bot.delete_message(update.effective_chat.id, context.user_data['msg_id_reminder'])
+    if any(memory.values()):
+        update.message.reply_text("Okay! I'll remind you when machine's ready.")
+    else:  # If the user did not tick any of the machines
+        update.message.reply_text("Hey you didn't pick any of the machines!")
 
-    # TO DO: set required jobs here using Jobs:
-    # https://python-telegram-bot.readthedocs.io/en/stable/telegram.ext.job.html?highlight=job
-    # Make the job ping backend repeatedly
+    # Check machines now
+    check_reminders(context)
 
     return SHOWING
+
+
+
+def check_reminders(context):
+    """Fetch finishing machines from backend and remind relevant users"""
+
+    reminders = context.bot_data["reminders"]
+    if len(reminders) == 0:
+        return
+
+    r = requests.get(BACKEND_URL)
+    # To actually change to get machines that are status=finishing
+    # r = requests.get(BACKEND_URL, params={})
+    all_machines = r.json()
+
+
+    text_template = "Wassup, machine {machine_name} is about to be done!"
+
+    # Consider rewriting this part to loop through users first (so each user only gets one message even
+    # if user has multiple machines)
+
+    for one in all_machines:
+        machine_id = one["id"]
+        if machine_id in reminders:
+            users_to_remind = reminders[machine_id]
+            for user in users_to_remind:
+                context.bot.send_message(
+                        chat_id=user,
+                        text=text_template.format(machine_name=str(machine_id))
+                )
+            print(f"I gotta send reminders to {reminders[machine_id]}")
+
+    # Also remove entry from dict once reminder is sent
+
+
+
+
 
 
 def err(update, context):
@@ -311,6 +340,7 @@ def main() -> None:
 
     # Other custom initialisation required
     dispatcher.bot_data["reminders"] = {}
+    dispatcher.job_queue.run_repeating(check_reminders, 30, 10)  # Check every 30 seconds (just for testing)
 
     # Start the Bot
     updater.start_polling()
